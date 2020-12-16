d018351ffbc2bba8db32ac3fad9145b691dba2fc
elton-felix <eltomfelix@gmail.com>
2020-12-16 20:25:45 -0300

    Enviando App

---
+++ src/server.js
@@ -0,0 +1,30 @@
+//importar dependencia
+const express = require('express');
+const path = require('path');
+const pages = require('./pages.js');
+
+
+//iniciando o express
+const server = express();
+server
+
+//utilizar body do req
+.use(express.urlencoded({extended:true}))
+
+//ultilizando os arquivos estaticos
+.use(express.static('public'))
+
+//configurar tamplate engine
+.set('views', path.join(__dirname, "views"))
+.set('view engine', 'hbs')
+
+//rotas da aplicação
+.get('/', pages.index)
+.get('/orphanage', pages.orphanage)
+.get('/orphanages', pages.orphanages)
+.get('/create-orphanage', pages.createOrphanage)
+.post('/save-orphanage',pages.saveOrphanage)
+
+
+//lligar o servidor
+server.listen(5500)
